85a990fa35dd4a6ba666f70623d0bf6dfd6aeed3
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2020-07-05 13:20:57 +0100

    Update BorrarLlave.bat

--- BorrarLlave.bat
+++ BorrarLlave.bat
@@ -1,5 +1,6 @@
 @echo off
 ssh pi@ip "ls -I 'excluido.ovpn' -I 'excluido2.ovpn' ~/ovpns/ | sed -e 's/\.ovpn$//'"
-
 SET /P "nombre=Introduce la llave a borrar: "
-ssh pi@ip "pivpn -r %nombre%.ovpn"
+cls
+ssh pi@ip "pivpn -r %nombre%"
+PAUSE
