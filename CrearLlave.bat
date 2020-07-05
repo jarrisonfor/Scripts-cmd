2d98bde4097c5c533b8797977e6328f838790f34
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2020-07-05 03:41:49 +0100

    Update CrearLlave.bat

--- CrearLlave.bat
+++ CrearLlave.bat
@@ -2,5 +2,5 @@
 
 SET /P "nombre=Introduce un nombre para la llave: "
 SET /P "password=Introduce una contraseña para la llave: "
-ssh pi@ip "pivpn -a -n %nombre% -p %password% -d 1080 | grep created"
+ssh pi@ip "pivpn -a -n %nombre% -p %password% -d 1080
 scp pi@ip:~/ovpns/%nombre%.ovpn ./
